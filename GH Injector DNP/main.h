d326697976b4399cd1d08960a88584b90570f6f5
owen <zxc88645@gmail.com>
2023-04-09 14:23:38 +0800

    修win11注入器

--- GH Injector DNP/main.h
+++ GH Injector DNP/main.h
@@ -5,7 +5,7 @@
 #include "DotNetInjection.h"
 
 #ifdef _WIN64
-#define GH_DNP_NAME "TF  Injector DNP - x64.dll"
+#define GH_DNP_NAME "TF Injector DNP - x64.dll"
 #else
-#define GH_DNP_NAME "TF  Injector DNP - x86.dll"
+#define GH_DNP_NAME "TF Injector DNP - x86.dll"
 #endif
